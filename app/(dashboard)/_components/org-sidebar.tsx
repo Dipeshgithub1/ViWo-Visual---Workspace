b2ecdd3c7e411c3f33d476d63703bb68993333d4
Dipesh Kumar <dipesh.kumar@iic.ac.in>
2024-06-27 11:52:56 +0530

    Initial commit

--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -33,7 +33,7 @@ export const OrgSidebar = () => {
             "font-semibold text-2xl",
             font.className,
           )}>
-            Board
+            Vi-Wo
           </span>
         </div>
       </Link>
